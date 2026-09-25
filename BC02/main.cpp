dd939e472be3ac5f815e8523a17ea9ddb6b924f0
wes te braak <wesbraak3@hotmail.com>
2026-09-25 09:50:49 +0200

    setup constructor and destructor

--- BC02/main.cpp
+++ BC02/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <cstdio>
+
+class SimpleClass {
+    public:
+        int simpleValue;
+        SimpleClass();
+        ~SimpleClass();
+};
+
+SimpleClass::SimpleClass() {
+    simpleValue = 0;
+    printf("SimpleClass was created\n");
+}
+
+SimpleClass::~SimpleClass() {
+    printf("SimpleClass was destroyed with value: %d\n", simpleValue);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    {
+        SimpleClass cl1;
+        cl1.simpleValue = 10;
+        printf("SimpleClass was created with value %d\n", cl1.simpleValue);
+    }
+
+    SimpleClass* sl2 = new SimpleClass();
+    sl2->simpleValue = 20;
+    delete sl2;
+
     return 0;
 }
